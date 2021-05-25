c8819ee5da469c936bf7c57d144fb075f4a4f2f6
Ran-n <ran-n@tutanota.com>
2021-05-25 23:27:51 +0200

    cambios

---
+++ blockchain.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# --*-- coding: utf-8 --*--
+#+ Autor:	Ran#
+#+ Creado:	25/05/2021 23:08:08
+#+ Editado:	25/05/2021 23:08:14
+
+from hashlib import sha256
+
+class Block:
+    pass
+
+class Blockchain:
+    pass
+
+
+def main():
+    print('ola')
+
+if __name__ == '__main__':
+    main()
