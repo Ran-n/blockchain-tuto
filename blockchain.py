7b9af774875b85ff7ce3943f34d734054c777d87
Ran-n <ran-n@tutanota.com>
2021-05-27 22:46:55 +0200

    part3

--- blockchain.py
+++ blockchain.py
@@ -2,7 +2,7 @@
 # --*-- coding: utf-8 --*--
 #+ Autor:	Ran#
 #+ Creado:	25/05/2021 23:08:08
-#+ Editado:	26/05/2021 23:06:09
+#+ Editado:	27/05/2021 22:46:30
 
 from hashlib import sha256
 
@@ -34,11 +34,12 @@ class Block:
 
 
 class BlockChain:
-    difficulty = 6
+    difficulty = 3
 
     def __init__(self, chain=[]):
         self.chain = chain
 
+    """
     def add(self, block):
         self.chain.append({
             'hash': block.hash(), 
@@ -47,10 +48,17 @@ class BlockChain:
             'data': block.data, 
             'nonce': block.nonce
             })
+    """
+    def add(self, block):
+        self.chain.append(block)
+
+    def remove(self, block):
+        self.chain.remove(block)
 
     def mine(self, block):
         try:
-            block.previous_hash = self.chain[-1].get('hash')
+            #block.previous_hash = self.chain[-1].get('hash')
+            block.previous_hash = self.chain[-1].hash()
         except IndexError:
             pass
 
@@ -60,6 +68,16 @@ class BlockChain:
                 break
             else:
                 block.nonce += 1
+
+    def isValid(self):
+        for i in range(1, len(self.chain)):
+            _previous = self.chain[i].previous_hash
+            _current = self.chain[i-1].hash()
+
+            if (_previous != _current) or (_current[:self.difficulty] != '0'*self.difficulty):
+                return False
+
+        return True
 
 
 def main():
@@ -73,9 +91,11 @@ def main():
 
     print(bc.chain)
     for block in bc.chain:
-        print()
         print(block)
 
+    bc.chain[2].data = 'new data'
+    bc.mine(bc.chain[2])
+    print(bc.isValid())
 
 
 if __name__ == '__main__':
